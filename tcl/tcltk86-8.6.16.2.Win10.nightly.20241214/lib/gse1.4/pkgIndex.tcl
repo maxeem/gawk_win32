9edb05d7e6c9c5d0965de181847843b40d721f77
Maksim Abuajamieh <eng.maksim@gmail.com>
2024-12-16 17:13:41 +0000

    tcl

---
+++ tcl/tcltk86-8.6.16.2.Win10.nightly.20241214/lib/gse1.4/pkgIndex.tcl
@@ -0,0 +1,15 @@
+package ifneeded gse 1.4 {
+    package provide gse 1.4
+    namespace eval gse {
+        variable tcl_release "2"
+        variable build_os "NT-10.0-20348"
+        variable build_kernelname "MINGW64_NT-10.0-20348"
+        variable build_kernelrelease "3.4.10.x86_64"
+        variable build_kernelversion "2024-04-08 18:11 UTC"
+        variable build_nodename "buildserver45"
+        variable build_machine "x86_64"
+        variable build_date "Sat, Dec 14, 2024  2:22:19 AM"
+        variable git_hash "c6ebdac62db7bb79bf13994a93290900213532cd"
+        variable gcc_version "3.2.0"
+    }
+}
